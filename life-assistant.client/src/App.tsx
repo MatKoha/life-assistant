fe273f545c818384c31d2179d04a030313dae992
MatKoha <matiaskohanevic@gmail.com>
2024-02-07 22:27:48 +0200

    add material mui and create dashboard component

--- life-assistant.client/src/App.tsx
+++ life-assistant.client/src/App.tsx
@@ -1,56 +1,44 @@
 import { useEffect, useState } from 'react';
+import Dashboard from './components/Dashboard';
 import './App.css';
+import { Grid } from '@mui/material';
 
-interface Forecast {
-    date: string;
-    temperatureC: number;
-    temperatureF: number;
-    summary: string;
-}
+//interface Forecast {
+//    date: string;
+//    temperatureC: number;
+//    temperatureF: number;
+//    summary: string;
+//}
 
 function App() {
-    const [forecasts, setForecasts] = useState<Forecast[]>();
+    //const [forecasts, setForecasts] = useState<Forecast[]>();
 
-    useEffect(() => {
-        populateWeatherData();
-    }, []);
-
-    const contents = forecasts === undefined
-        ? <p><em>Loading... Please refresh once the ASP.NET backend has started. See <a href="https://aka.ms/jspsintegrationreact">https://aka.ms/jspsintegrationreact</a> for more details.</em></p>
-        : <table className="table table-striped" aria-labelledby="tabelLabel">
-            <thead>
-                <tr>
-                    <th>Date</th>
-                    <th>Temp. (C)</th>
-                    <th>Temp. (F)</th>
-                    <th>Summary</th>
-                </tr>
-            </thead>
-            <tbody>
-                {forecasts.map(forecast =>
-                    <tr key={forecast.date}>
-                        <td>{forecast.date}</td>
-                        <td>{forecast.temperatureC}</td>
-                        <td>{forecast.temperatureF}</td>
-                        <td>{forecast.summary}</td>
-                    </tr>
-                )}
-            </tbody>
-        </table>;
+    //useEffect(() => {
+    //    populateWeatherData();
+    //}, []);
 
     return (
         <div>
-            <h1 id="tabelLabel">Weather forecast</h1>
-            <p>This component demonstrates fetching data from the server.</p>
-            {contents}
+            <Dashboard>
+                <Grid item xs={12} md={6}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12}>
+                            Moi
+                        </Grid>
+                        <Grid item xs={12}>
+                            Hei
+                        </Grid>
+                    </Grid>
+                </Grid>
+            </Dashboard>
         </div>
     );
 
-    async function populateWeatherData() {
-        const response = await fetch('weatherforecast');
-        const data = await response.json();
-        setForecasts(data);
-    }
+    //async function populateWeatherData() {
+    //    const response = await fetch('weatherforecast');
+    //    const data = await response.json();
+    //    setForecasts(data);
+    //}
 }
 
 export default App;
