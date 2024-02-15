f6243868286ff48ebc8527f303ef60781dba77cb
MatKoha <matiaskohanevic@gmail.com>
2024-02-15 23:38:19 +0200

    remove react strict mode

--- life-assistant.client/src/main.tsx
+++ life-assistant.client/src/main.tsx
@@ -10,7 +10,5 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
     <App />
-  </React.StrictMode>,
 )
